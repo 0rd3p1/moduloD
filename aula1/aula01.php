a8c789e3fba43dbb4b28555a75f358d17c4291d3
Pedro Henrique Fernandes de Oliveira <fernandesdeoliveira261@gmail.com>
2025-01-28 21:02:24 -0300

    teste

---
+++ aula1/aula01.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = "slk";
+
+var_dump($nome);
+
+echo "Hello World";
+
+if ($nome == "slk") {
+    echo "Hello Wolrd";
+} else if ($nome == "kls") {
+    echo "sla";
+} else {
+    echo "sai";
+}
+
+?>
