4327e4369e6ba59e838d08af3f319f0d17ea7898
Pedro Henrique Fernandes de Oliveira <fernandesdeoliveira261@gmail.com>
2025-02-11 22:00:27 -0300

    Execs Classes

---
+++ AprendendoPHP/aula08/User.php
@@ -0,0 +1,105 @@
+<?php
+
+class User {
+
+    public int $id;
+    public $nome;
+    public $senha;
+    public $email;
+
+    /*
+    public function __construct ($i, $n, $s, $e) {
+        $this -> id = $i;
+        $this -> nome = $n;
+        $this -> senha = $s;
+        $this -> email = $e;
+    }
+    */
+
+    public function add ($n, $s, $e, $c, $users): array {
+        if (!$this -> authEmail ($e, $users)) {
+            echo "\tEmail ja cadastrado\t";
+            return $users;
+        }
+
+        $u = new User(/*$n, $s, $e, $users, $c*/);
+        
+        $u -> id = $c;
+        $u -> nome = $n;
+        $u -> senha = $s;
+        $u -> email = $e;
+        $users[] = $u;
+
+        echo "\n\tUsuario cadastrado!\n";
+        return $users;
+    }
+
+    public function authEmail ($e, $users) {
+        foreach ($users as $u) {
+            if ($u -> email == $e) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function del ($e, $users): array {
+        for ($i = 0; $i < count($users); $i++) {
+            if ($e == $users[$i] -> email) {
+                unset($users[$i]);
+
+                echo "\nUsuario excluido!\n";
+                return $users;
+            }
+        }
+
+        echo "\n\tNão foi possivel excluir!\n";
+        return $users;
+    }
+
+    public function alt ($n, $s, $e, $users): array {
+        for ($i = 0; $i < count($users); $i++) {
+            if ($e == $users[$i] -> email) {
+                $users[$i] -> nome = $n;
+                $users[$i] -> senha = $s;
+                $users[$i] -> email = $e;
+
+                echo "\n\tUsuario alterado!\n";
+                return $users;
+            }
+        }
+
+        echo "\n\tUsuario nao encontrado!!\n";
+        return $users;
+    }
+
+    public function getByEmail ($e, $users): array {
+        for ($i = 0; $i < count($users); $i++) {
+            if ($e == $users[$i] -> email) {
+                echo "\n--------------------------------- \n";
+                echo "ID: " . $users[$i] -> id . "\n";
+                echo "Nome: " . $users[$i] -> nome . "\n";
+                echo "Email: " . $users[$i] -> email . "\n";
+                echo "Senha: " . $users[$i] -> senha . "\n";
+                echo "---------------------------------\n\n";
+                return $users;
+            }
+        }
+
+        echo "\n\tUsuario nao encontrado!\n";
+        return $users;
+    }
+
+    public function getAll ($users): void {
+        foreach ($users as $u) {
+            echo "\n--------------------------------- \n";
+            echo "ID: " . $u -> id . "\n";
+            echo "Nome: " . $u -> nome . "\n";
+            echo "Email: " . $u -> email . "\n";
+            echo "Senha: " . $u -> senha . "\n";
+            echo "---------------------------------\n\n";
+        }
+    }
+}
+
+?>
---
+++ AprendendoPHP/aula08/index.php
@@ -0,0 +1,85 @@
+<?php
+
+include 'User.php';
+
+$c = 1;
+$users = [];
+$op = 0;
+$u = new User(/*$i, $n, $s, $e*/);
+
+while ($op != 6) {
+
+    echo "\n\tBem vindo ao Bizzonhos solucoes\n";
+    echo "\nEscolha uma opção abaixo";
+    echo "\n1 - Cadastrar";
+    echo "\n2 - Excluir";
+    echo "\n3 - Alterar";
+    echo "\n4 - Buscar por Email";
+    echo "\n5 - Mostrar Todos";
+    echo "\n6 - Sair";
+    echo "\nSua escolha: ";
+    $op = readline();
+
+    switch ($op) {
+
+        case 1:
+            echo "\nInforme seu nome: ";
+            $n = readline();
+            echo "\nInforme seu email: ";
+            $e = readline();
+            echo "\nInforme sua senha: ";
+            $s = readline();
+
+            if ($c == count($users)) {
+                $c++;
+            }
+
+            $users =  $u -> add($n, $e, $s, $c, $users);
+            
+            break;
+
+        case 2:
+            echo "\nInforme o email do usuario que deseja excluir: ";
+            $e = readline();
+            
+            $users = $u -> del($e, $users);
+            $c--;
+            break;
+
+        case 3:
+            echo "\nInforme o email do usuario que deseja alterar: ";
+            $e = readline();
+
+            echo "\n\nDigite o novo nome: ";
+            $n = readline();
+
+            echo "\n\nDigite a nova senha: ";
+            $s = readline();
+
+            echo "\n\nDigite o novo email: ";
+            $e = readline();
+
+            $users = $u -> alt($n, $s, $e, $users);
+
+        case 4:
+            echo "\nInforme o email do usuario que deseja buscar: ";
+            $e = readline();
+            
+            $users = $u -> getByEmail($e, $users);
+            break;
+        
+        case 5:
+            $u -> getAll($users);
+            break;
+
+        case 6:
+            echo "\n\tAte mais!!!\n\n";
+            exit;
+
+        default:
+            echo "\n\tEscolha uma das opcoes seu demente!!\n";
+            break;
+    }
+}
+
+?>
